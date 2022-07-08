fc93bf57c87ef66044d68dab03d55df379a1469c
mateusnagelo <mateusgenius1234@hotmail.com>
2022-07-08 20:28:34 -0300

    preparação do html para receber o js

---
+++ scripts/script-login.js
@@ -0,0 +1,9 @@
+// function logar() {
+
+//     var email = document.getElementById('email').value;
+//     var password = document.getElementById('password').value;
+
+
+//     if (email == "admin" && senha == "1")
+//         alert('Sucesso')
+// }
