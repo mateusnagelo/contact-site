a330b59ad2d8ea5e2377e5ffc9737740fd05999a
mateusnagelo <mateusgenius1234@hotmail.com>
2022-07-10 10:38:20 -0300

    inclusao função visualizar senha js

--- scripts/script-login.js
+++ scripts/script-login.js
@@ -33,11 +33,11 @@ document.addEventListener("keypress", function(e) {
 
 
 function mouseoverPass(obj) {
-    var obj = document.getElementById('password');
+    var obj = document.getElementById('senha');
     obj.type = "text";
 }
 
 function mouseoutPass(obj) {
-    var obj = document.getElementById('password');
+    var obj = document.getElementById('senha');
     obj.type = "password";
 }
